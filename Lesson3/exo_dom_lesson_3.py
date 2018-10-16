c5a452f39e4035cada8ee4cefbb13e7579a4f033
Anatoli_deBRADKE <anatoli-debradke@telecom-paristech.fr>
2018-10-16 15:25:14 +0200

    add exercice 3 dom

---
+++ Lesson3/exo_dom_lesson_3.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+import json
+
+userID = "Anatoli-deBRADKE"
+token = "6711aaa1d2de1d3d35865226674c8fb731fe24df"
+
+def build_soup_from_url(url):
+    page = requests.get(url)
+    html = page.text
+    soup = BeautifulSoup(html,"html.parser")
+    return soup
+
+def get_top_contributor():
+    url = "https://gist.github.com/paulmillr/2657075"
+    soup = build_soup_from_url(url)
+    top_contributor = []
+    table = soup.find('table').find_all('tr')
+    for index in range(1,len(table)):
+        contributor_pseudo = table[index].find('a').getText()
+        top_contributor.append(contributor_pseudo)
+    return top_contributor
+
+
+def get_contributor_stars(contrib):
+    url = "https://api.github.com/users/" + contrib + "/repos"
+    res = requests.get(url, auth = (userID,token))
+    reponse_object = json.loads(res.text)
+    star_count = 0
+    for repo in reponse_object:
+        star_count += int(repo['stargazers_count'])
+    if (len(reponse_object) == 0):
+        return 0
+    else:
+        return round(star_count/len(reponse_object), 2)
+
+def get_best_contributor():
+    top_contributor = get_top_contributor()
+    best_contributor = {}
+    for contributor in top_contributor:
+        best_contributor[contributor] = get_contributor_stars(contributor)
+    sorted_best_contributor = sorted(best_contributor.items(), key=lambda kv: kv[1], reverse=True)
+    return sorted_best_contributor
+
+print(get_best_contributor())
